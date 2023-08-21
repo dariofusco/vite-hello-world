7134875695096082c5e5b5140a1f654284085f09
dariofusco <fdario@hotmail.it>
2023-08-21 16:03:26 +0200

    bonus inserisce jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container bg-light">
+    <h1 class="display-4">Jumbotron</h1>
+    <p class="lead">Questo è un Jumbotron.</p>
+  </div>
+</div>
+</template>
+
+<style>
+
+</style>
